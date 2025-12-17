50a0fc7f10987d487c7989ee1b077cbb7d307f4d
Manvendra Singh <manvendrasinghpratap@icloud.com>
2025-12-17 22:04:37 +0530

    new changes

---
+++ resources/views/layouts/frontend/css.blade.php
@@ -0,0 +1,11 @@
+<!------   --->
+<link href="{{ asset('assets/vendor/line-awesome/line-awesome/line-awesome/css/line-awesome.min.css') }}" rel="stylesheet" />
+<!-- Plugins CSS File -->
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/plugins/owl-carousel/owl.carousel.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/plugins/magnific-popup/magnific-popup.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/plugins/jquery.countdown.css') }}" rel="stylesheet" />
+<!-- Main CSS File -->
+<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/skins/skin-demo-6.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/demos/demo-6.css') }}" rel="stylesheet" />
